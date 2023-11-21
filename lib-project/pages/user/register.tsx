3c30a239aed5a571c84508f6ccb3765fff376d59
Sabo <57690741+carloskbonet@users.noreply.github.com>
2023-11-21 17:26:02 -0300

    commit 21.11

--- lib-project/pages/user/register.tsx
+++ lib-project/pages/user/register.tsx
@@ -1,21 +1,70 @@
 import styles from '@/styles/register.module.css';
+import { useState } from 'react';
+import { useRouter } from 'next/router';
 
 export default function registerPage() {
 
+    const [formData , setFormData] = useState({
+        name: '',
+        email: '',
+        username: '',
+        password: '',
+        confirmPassword: ''
+    });
+
+    const router = useRouter();
+
+    function handleFormEdit(event: any , fieldName:string) {
+        setFormData({
+            ...formData,
+            [fieldName] : event.target.value
+        });
+    }
+
+    async function formSubmit(event:any) {
+        event.preventDefault();
+
+        try {
+
+            const response = await fetch('/api/action/user/create', {
+                method: 'POST',
+                headers: {'Content-type' : 'application/json'},
+                body: JSON.stringify(formData)
+            });
+            
+            const responseJson = await response.json();
+
+            console.log(response.status);
+            console.log(responseJson);
+
+            if ( response.status != 201 ) {
+                throw new Error(responseJson.message);
+            }
+            else {
+                alert('Account Created');
+                router.push('/user/login');
+            }
+
+        }
+        catch( err:any ) {
+            alert(err.message);
+        }
+    }
+
     return (
         <main className={styles.main}>
-            <form className={styles.formulario}>
+            <form className={styles.formulario} onSubmit={formSubmit} >
                 <div className={styles.form_container}>
 
-                    <input type="text" placeholder="Digite o seu nome" />
+                    <input type="text" placeholder="Digite o seu nome" onChange={(event) => { handleFormEdit(event, 'name') } }  value={formData.name} />
                     <br />
-                    <input type="email" placeholder="Email" min={9} max={64} required />
+                    <input type="email" placeholder="Email" onChange={(event) => { handleFormEdit(event, 'email') } } value={formData.email} required />
                     <br />
-                    <input type="text" placeholder="Usuário" min={5} max={32} required />
+                    <input type="text" placeholder="Usuário" onChange={(event) => { handleFormEdit(event, 'username') } } value={formData.username} required />
                     <br />
-                    <input type="password" placeholder="Senha" min={6} max={32} required />
+                    <input type="password" placeholder="Senha" onChange={(event) => { handleFormEdit(event, 'password') } } value={formData.password} required />
                     <br />
-                    <input type="password" placeholder="Confirmação de Senha" min={6} max={32} required />
+                    <input type="password" placeholder="Confirmação de Senha" onChange={(event) => { handleFormEdit(event, 'confirmPassword') } } value={formData.confirmPassword} required />
                     <br />
 
                     <button className={styles.form_btn}>Enviar</button>
